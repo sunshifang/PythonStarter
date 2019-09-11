20d6e4a8f3b2647ab22472dc288c147ce4f772cd
sunshifang <sun_shi_fang@163.com>
2019-09-11 15:22:14 +0800

    增加函数章节

---
+++ 09_func.py
@@ -0,0 +1,11 @@
+def area(width, height):
+    # 计算面积函数
+    return width * height
+
+def print_welcome(name):
+    print("Welcome",name)
+
+
+#调用函数
+print_welcome("Runoob")
+print("width = 4, height = 5, area = ",area(4,5))
