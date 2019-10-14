aa87de56703d94404bbcdc0248b7acf87f6c84eb
sunshifang <sun_shi_fang@163.com>
2019-10-14 11:10:15 +0800

    201910141110

--- 31_NumPy_CreateArray.py
+++ 31_NumPy_CreateArray.py
@@ -224,6 +224,151 @@ print(a.T)
 print()
 
 print("3)numpy.rollaxis向后滚动特定的轴到一个特定的位置")
+a = np.arange(8).reshape(2,2,2)
+print('原数组：')
+print(a)
+print("执行np.rollaxis(a,2)后：")
+print(np.rollaxis(a,2))
+print()
+print("执行np.rollaxis(a,2,1)后：")
+print(np.rollaxis(a,2,1))
+print()
+
+print("4)numpy.swapaxes 函数用于交换数组的两个轴")
+a = np.arange(8).reshape(2,2,2)
+print('原数组：')
+print(a)
+print("执行np.swapaxes(a,2,0)后：")
+print(np.swapaxes(a,2,0))
+print()
+
+print("3.修改数组维度")
+print("1)numpy.broadcast将一个数组广播到另一个数组")
+x = np.array([[1],[2],[3]])
+y = np.array([4,5,6])
+print("x=[[1],[2],[3]]")
+print("y=[4,5,6]")
+print()
+print("对y广播x b=n.broadcast(x,y)")
+b = np.broadcast(x,y)
+print("b拥有iterator属性，基于自身组件的迭代器元组")
+r,c = b.iters
+counter = 0
+while counter<9:
+    print(next(r),next(c))
+    counter += 1
+print()
+
+print('广播对象的形状：')
+print(b.shape)
+print()
+
+print("手动使用broadcast蒋x与y相加：")
+b = np.broadcast(x,y)
+c = np.empty(b.shape)
+print(c.shape)
+print()
+c.flat = [u+v for (u,v) in b]
+
+print('调用flat函数：')
+print(c)
+print()
+
+print("x与y的和：")
+print(x+y)
+print()
+print("……")
+print()
+
+print("4.连接数组")
+print("1)numpy.concatenate()沿指定轴连接相同形状的两个或多个数组")
+a = np.array([[1,2],[3,4]])
+print('第一个数组：')
+print(a)
+b = np.array([[5,6],[7,8]])
+print('第二个数组：')
+print(b)
+print('沿轴0连接两个数组：')
+print(np.concatenate((a,b)))
+print('沿轴1连接两个数组：')
+print(np.concatenate((a,b),axis=1))
+print()
+
+print("5.分割数组")
+print("1)numpy.split(ary,indices_or_sections,axis)")
+a = np.arange(9)
+print('第一个数组：')
+print(a)
+print('np.split(a,[4,7])将数组在一维数组中表明的位置分割：')
+b = np.split(a,[4,7])
+print(b)
+print('原数组不变')
+print(a)
+print()
+
+print('6.数组元素的添加与删除')
+print('1)numpy.resize返回指定形状的新数组')
+a = np.array([[1,2,3],[4,5,6]])
+print('第一个数组：')
+print(a)
+print('第一个数组的形状：')
+print(a.shape)
+print('执行np.resize(a,(3,2))后：')
+b = np.resize(a,(3,2))
+print('第二个数组：')
+print(b)
+print('第二个数组的形状：')
+print(b.shape)
+print('修改第二个数组的大小b=np.resize(a,(3,3))：')
+b = np.resize(a,(3,3))
+print(b)
+print()
+print('修改第二个数组的大小b=np.resize(a,(4,3))：')
+b = np.resize(a,(4,3))
+print(b)
+
+print('修改第二个数组的大小b=np.resize(a,(3,4))：')
+b = np.resize(a,(3,4))
+print(b)
+print()
+
+print('7.NumPy位运算')
+print('1）numpy.bitwise_and对数组中整数的二进制形式执行位与运算')
+print('13和17的二进制形式：')
+a,b = 13,17
+print(bin(a), bin(b))
+print('13和17的位与：')
+print(np.bitwise_and(13,17))
+print()
+
+print('8.NumPy字符串函数')
+print('1)numpy.char.add连接两个字符串')
+print("连接两个字符串np.char.add(['hello'],['xyz'])")
+print(np.char.add(['hello'],['xyz']))
+print()
+
+print('9.NumPy数学函数')
+print('1)三角函数sin(),cos(),tan()')
+a = np.array([0,30,45,60,90])
+print('打印角度的数组(通过乘pi/180转化为弧度)：')
+print(a)
+print()
+print('数组中角度的正弦值：')
+print(np.sin(a*np.pi/180))
+print('数组中角度的余弦值：')
+print(np.cos(a*np.pi/180))
+print('数组中角度的正切值：')
+print(np.tan(a*np.pi/180))
+
+
+
+      
+
+
+
+
+
+
 
 
 
